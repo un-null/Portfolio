c7cdb104a52326bb9c4da0e9f8ee22deda301bfb
null <keita@gaogao.asia>
2023-10-07 15:06:27 +0900

    ➕ setup Notion API

---
+++ src/types/env.d.ts
@@ -0,0 +1,8 @@
+declare namespace NodeJS {
+  interface ProcessEnv {
+    readonly NOTION_TOKEN: string;
+    readonly NOTION_DATABASE_ID_WORKS: string;
+    readonly NOTION_DATABASE_ID_FORM: string;
+    readonly NEXT_PUBLIC_HOSTNAME: string;
+  }
+}
