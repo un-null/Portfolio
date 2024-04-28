1493976035eb1e63ecc2b72d90d58719d9819588
null <keita@gaogao.asia>
2024-04-29 07:34:57 +0900

    🚸 add back link #4

--- src/app/blog/(id)/[id]/page.tsx
+++ src/app/blog/(id)/[id]/page.tsx
@@ -1,3 +1,6 @@
+import Link from "next/link";
+
+import { ArrowLeft } from "@/components/icons/left-arrow";
 import { BlockToJSX } from "@/components/notion/block-to-jsx";
 import { getPageById } from "@/utils/notion";
 
@@ -6,7 +9,16 @@ export default async function BlogId({ params }: { params: { id: string } }) {
 
   return (
     <div>
-      <ul className="mx-auto text-sm">
+      <Link
+        href={"/blog"}
+        className="text-gray-dim mt-4 flex items-center gap-2 text-sm hover:text-graya-normal"
+      >
+        <span>
+          <ArrowLeft />
+        </span>
+        戻る
+      </Link>
+      <ul className="mx-auto mt-8 text-sm">
         {blocks.map((block) => (
           <BlockToJSX key={block.id} block={block} />
         ))}
